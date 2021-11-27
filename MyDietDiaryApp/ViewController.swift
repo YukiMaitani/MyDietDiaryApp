5ec05a5427293d515742e2ab8efa81ee1a51a9e8
YukiMaitani <fightzoi1997@gmail.com>
2021-11-28 05:26:14 +0900

    Initial Commit

---
+++ MyDietDiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDietDiaryApp
+//
+//  Created by 米谷裕輝 on 2021/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
