5017b5cf5efd53cd6f34121ec40830f1be0d2b47
YukiMaitani <fightzoi1997@gmail.com>
2021-12-01 08:07:48 +0900

    RealmSwiftのインストールとモデルの作成

--- MyDietDiaryApp/EditorViewController.swift
+++ MyDietDiaryApp/EditorViewController.swift
@@ -9,4 +9,21 @@ import UIKit
 
 class EditorViewController:UIViewController{
     
+    @IBOutlet weak var inputWeightTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureWightTextField()
+    }
+    @objc func didTapDone(){
+        view.endEditing(true)
+    }
+    func configureWightTextField(){
+        let toolBarRect = CGRect.init(x: 0, y: 0, width: view.frame.size.width, height: 35)
+        let toolBar = UIToolbar(frame: toolBarRect)
+        let toolBarItem = UIBarButtonItem.init(barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
+        toolBar.setItems([toolBarItem], animated: true)
+        inputWeightTextField.inputAccessoryView = toolBar
+    }
 }
+
+
---
+++ MyDietDiaryApp/WeightRecord.swift
@@ -0,0 +1,18 @@
+//
+//  WeightRecord.swift
+//  MyDietDiaryApp
+//
+//  Created by 米谷裕輝 on 2021/12/01.
+//
+
+import Foundation
+import RealmSwift
+
+class weightRecord:Object{
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    @objc dynamic var id:String = UUID().uuidString
+    @objc dynamic var weight:Double = 0
+    @objc dynamic var date:Date = Date()
+}
