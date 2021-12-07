5fc059040e001a08db77588b031caa38a42e6032
YukiMaitani <fightzoi1997@gmail.com>
2021-12-07 12:04:32 +0900

    グラフ画面の表示と見た目の調整

---
+++ MyDietDiaryApp/GraphDataTitleFormatter.swift
@@ -0,0 +1,22 @@
+//
+//  GraphDataTitleFormatter.swift
+//  MyDietDiaryApp
+//
+//  Created by 米谷裕輝 on 2021/12/07.
+//
+
+import Foundation
+import Charts
+
+class GraphDataTitleFormatter:IAxisValueFormatter{
+    var dateList:[Date] = []
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let index = Int(value)
+        guard dateList.count > index else {return ""}
+        let targetDate = dateList[index]
+        //戻り値がString型なのでdateformatterでDate型からString型に直している
+        let formatter = DateFormatter()
+        formatter.dateFormat = "M/d"
+        return formatter.string(from: targetDate)
+    }
+}
